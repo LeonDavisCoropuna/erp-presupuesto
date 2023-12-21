796c750ac869dbf8d90a425afb569c5b6cd76c03
OwenRoque <103952714+OwenRoque@users.noreply.github.com>
2023-12-21 12:36:24 -0500

    App.tsx Actualizado (#14)
    * push data
    
    * update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,39 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Navigate, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Login from "./pages/login/Login";
+import RouteNotFound from "./utils/RouteNotFound";
+import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Suspense } from "react";
 
+// Importa tus componentes que deseas cargar de forma diferida
+import System from "./pages/private/system/System";
 function App() {
-
   return (
-    <div className='p-5 bg-red-700 w-full text-white'>Holaaa</div>
-  )
+    <div className="App">
+      <Provider store={store}>
+        <Suspense fallback={<div>...Wait</div>}>
+          <BrowserRouter>
+            <RouteNotFound>
+              <Route path="/" element={<Navigate to="home" />} />
+              <Route path="home" element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="system/*"
+                element={
+                  <ProtectedRoute>
+                    <System />
+                  </ProtectedRoute>
+                }
+              />
+            </RouteNotFound>
+          </BrowserRouter>
+        </Suspense>
+      </Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
